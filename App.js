a62b79235b7a4f183918f7d986292e3b6e150298
Priteshkamde <priteshkamde24@gmail.com>
2021-12-30 18:15:11 +0530

    Adding Initial Base Structure

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState("prit")
+  const [age, setAge] = useState(24)
+
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        backgroundColor='pink'
+      />
+
+      <Text> Enter Name </Text>
+      <TextInput style={styles.inputStyle}
+        placeholder='John Doe'
+        onChangeText={(val) => setName(val)}
+      />
+
+
+      <Text> Enter Age </Text>
+      <TextInput style={styles.inputStyle}
+        keyboardType='numeric'
+        placeholder='9999'
+        onChangeText={(val) => setAge(val)}
+      />
+
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello {name} | {age}</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +39,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+    marginTop: 50,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  inputStyle: {
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 10,
+    margin: 10,
+    width: 200,
+  }
 });
